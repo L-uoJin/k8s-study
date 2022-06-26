0da202b18e5283835cf5ea1e8e405f011e473a21
yezhidi <38373040+yezhidi@users.noreply.github.com>
2022-06-26 20:08:22 +0800

    Add files via upload
    更新metrics 增加路由

--- module2/router/router.go
+++ module2/router/router.go
@@ -7,4 +7,6 @@ import (
 
 func InitRouter(mux *http.ServeMux)  {
 	mux.HandleFunc("/healthz", controller.Healthz)
+	mux.HandleFunc("/rand", controller.Rand)
+	mux.HandleFunc("/healthz", controller.Healthz)
 }
