2658aeb7c6255995ed4f78b3c890023573a2227e
yezhidi <38373040+yezhidi@users.noreply.github.com>
2022-06-26 20:34:35 +0800

    Add files via upload
    增加metrics 路由

--- module2/router/router.go
+++ module2/router/router.go
@@ -1,6 +1,7 @@
 package router
 
 import (
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"module2/controller"
 	"net/http"
 )
@@ -8,5 +9,6 @@ import (
 func InitRouter(mux *http.ServeMux)  {
 	mux.HandleFunc("/healthz", controller.Healthz)
 	mux.HandleFunc("/rand", controller.Rand)
+	mux.Handle("/metrics", promhttp.Handler())
 	mux.HandleFunc("/healthz", controller.Healthz)
 }
